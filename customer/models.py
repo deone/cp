7b68dd93c5eb63f6c9647e15b988a17f64476855
Oladayo Osikoya <alwaysdeone@gmail.com>
2020-11-24 20:25:40 +0000

    Customer models.

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=11)
+
+class RecipientAccount(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    _type = models.CharField(max_length=6)
+    name = models.CharField(max_length=50, null=True, blank=True) # this can be null since we can't get wallet names for now
+    number = models.CharField(max_length=10)
+    confirm_number = models.CharField(max_length=10, null=True)
+    provider_code = models.CharField(max_length=15)
+    provider_name = models.CharField(max_length=50)
