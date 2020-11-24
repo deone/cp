dd98254750e72a0886be8a12cc2853183ed9de1a
Oladayo Osikoya <alwaysdeone@gmail.com>
2020-11-24 20:18:18 +0000

    Create database and add settings.

--- cp/settings.py
+++ cp/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'cp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cp_dev',
+        'USER': 'cp_admin',
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
---
+++ db.sql
@@ -0,0 +1,2 @@
+create database cp_dev;
+grant all on cp_dev.* to 'cp_admin'@'localhost' identified by 'cpAdm1n';
