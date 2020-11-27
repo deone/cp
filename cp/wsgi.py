d0137ed37f95914e7a39e7191370a78d5a319d0d
Oladayo Osikoya <alwaysdeone@gmail.com>
2020-11-27 17:37:39 +0000

    Read production settings.

--- cp/wsgi.py
+++ cp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cp.settings_production')
 
 application = get_wsgi_application()
