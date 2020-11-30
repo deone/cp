77fb38a6f49a0b88611710958d4b386f65633855
Oladayo Osikoya <alwaysdeone@gmail.com>
2020-11-30 15:55:54 +0000

    Enable Rate in admin. Set IDs on amount fields. Display sample rate in template.

--- transaction/forms.py
+++ transaction/forms.py
@@ -6,15 +6,18 @@ FORM_CONTROL_CLASS = 'form-control'
 
 class TransactionForm(forms.Form):
     source_amount = forms.CharField(label='You send', widget=forms.TextInput(attrs={
-        'class': '{} {}'.format('autonumeric', FORM_CONTROL_CLASS),
-        'autofocus': True
+        'class': FORM_CONTROL_CLASS,
+        'id': 'source_amount',
+        'autofocus': True,
+        'value': '1000',
     }))
     source_currency = forms.ChoiceField(label='Currency I have', widget=forms.Select(attrs={
         'class': FORM_CONTROL_CLASS
     }), choices=SOURCE_CURRENCIES)
     dest_amount = forms.CharField(label='Recipient gets', widget=forms.TextInput(attrs={
-        'class': FORM_CONTROL_CLASS
+        'class': FORM_CONTROL_CLASS,
+        'id': 'dest_amount',
     }))
     dest_currency = forms.ChoiceField(label="Recipient's currency", widget=forms.Select(attrs={
-        'class': FORM_CONTROL_CLASS
+        'class': FORM_CONTROL_CLASS,
     }), choices=DEST_CURRENCIES)
