067fe4070f576f953005db5da9b2334cbb67aac2
Oladayo Osikoya <alwaysdeone@gmail.com>
2020-12-07 17:10:16 +0000

    Update settings.

--- cp/settings_production.py
+++ cp/settings_production.py
@@ -26,3 +26,11 @@ DATABASES = {
 }
 
 STATIC_ROOT = '/home/sdarko/webapps/cashpipe_static'
+
+FW_INCISIA_SEC_KEY = os.environ['FW_INCISIA_PROD_SEC_KEY']
+FW_INCISIA_PUB_KEY = os.environ['FW_INCISIA_PROD_PUB_KEY']
+
+FW_TRANSFER_URL = 'https://api.ravepay.co/v2/gpx/transfers/create'
+FW_BANKS_URL = 'https://api.ravepay.co/v2/banks/'
+FW_CREATE_BENEFICIARY_URL = 'https://api.ravepay.co/v2/gpx/transfers/beneficiaries/create'
+FW_PAYMENT_URL = 'https://api.ravepay.co/flwv3-pug/getpaidx/api/v2/hosted/pay'
