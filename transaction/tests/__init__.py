e5a0788d2a5f51f137e78e7c09b6df57041ffe60
Oladayo Osikoya <alwaysdeone@gmail.com>
2020-12-10 13:40:46 +0000

    Refactor tests.

--- transaction/tests/__init__.py
+++ transaction/tests/__init__.py
@@ -9,85 +9,103 @@ from decimal import Decimal
 class TestNGNToGHSTransaction(TestCase):
     def setUp(self):
         super().setUp()
-        self.ngn_to_ghs_transaction = Transaction.objects.create(transaction_id='CSHP201208155232267518')
+        self.transaction = Transaction.objects.create(
+            transaction_id='{}{}'.format('TMNY', timezone.now().strftime('%y%m%d%H%M%S%f')))
 
         # Create inflow
-        source_amount = Decimal('10000')
-        source_currency = 'NGN'
-
         Inflow.objects.create(
-            transaction=self.ngn_to_ghs_transaction,
-            currency=source_currency,
-            amount=source_amount,
+            transaction=self.transaction,
+            currency='NGN',
+            amount=Decimal('10000'),
         )
 
         # Create outflow
-        dest_amount = Decimal('123.45')
-        dest_currency = 'GHS'
-
         Outflow.objects.create(
-            transaction=self.ngn_to_ghs_transaction,
-            currency=dest_currency,
-            amount=dest_amount,
+            transaction=self.transaction,
+            currency='GHS',
+            amount=Decimal('123.45'),
         )
 
     def tearDown(self):
-        self.ngn_to_ghs_transaction.delete()
+        self.transaction.delete()
 
 class UpdatedTestNGNToGHSTransaction(TestNGNToGHSTransaction, TestCustomer):
     def setUp(self):
         super().setUp()
         # Update transaction
-        self.transaction_id = '{}{}'.format(
-            'SMNY', timezone.now().strftime('%y%m%d%H%M%S'))
-        self.ngn_to_ghs_transaction.user = self.customer.user
-        self.ngn_to_ghs_transaction.transaction_id = self.transaction_id
-        self.ngn_to_ghs_transaction.save()
+        self.transaction.user = self.customer.user
+        self.transaction.save()
 
         # Update outflow
-        self.ngn_to_ghs_transaction.outflow.dest_account_provider_code = 'mtn'
-        self.ngn_to_ghs_transaction.outflow.dest_account_number = '0546789100'
-        self.ngn_to_ghs_transaction.outflow.save()
+        self.transaction.outflow.dest_account_provider_code = 'mtn'
+        self.transaction.outflow.dest_account_number = '0546789100'
+        self.transaction.outflow.save()
 
 class TestGHSToNGNTransaction(TestCase):
     def setUp(self):
         super().setUp()
-        self.ghs_to_ngn_transaction = Transaction.objects.create(transaction_id='CSHP201208155232267519')
+        self.transaction = Transaction.objects.create(
+            transaction_id='{}{}'.format('TMNY', timezone.now().strftime('%y%m%d%H%M%S%f')))
 
         # Create inflow
-        source_currency = 'GHS'
-        source_amount = Decimal('100')
-
         Inflow.objects.create(
-            transaction=self.ghs_to_ngn_transaction,
-            currency=source_currency,
-            amount=source_amount,
+            transaction=self.transaction,
+            currency='GHS',
+            amount=Decimal('100'),
         )
 
         # Create outflow
-        dest_amount = Decimal('7813')
-        dest_currency = 'GHS'
-
         Outflow.objects.create(
-            transaction=self.ghs_to_ngn_transaction,
-            currency=dest_currency,
-            amount=dest_amount,
+            transaction=self.transaction,
+            currency='GHS',
+            amount=Decimal('7813'),
         )
 
     def tearDown(self):
-        self.ghs_to_ngn_transaction.delete()
+        self.transaction.delete()
 
 class UpdatedTestGHSToNGNTransaction(TestGHSToNGNTransaction, TestCustomer):
     def setUp(self):
         super().setUp()
         # Update transaction
-        self.transaction_id = '{}{}'.format(
-            'SMNY', timezone.now().strftime('%y%m%d%H%M%S'))
-        self.ghs_to_ngn_transaction.user = self.customer.user
-        self.ghs_to_ngn_transaction.transaction_id = self.transaction_id
-        self.ghs_to_ngn_transaction.save()
+        self.transaction.user = self.customer.user
+        self.transaction.save()
 
         # Update outflow
-        self.ghs_to_ngn_transaction.outflow.dest_account_provider_code = '044'
-        self.ghs_to_ngn_transaction.outflow.dest_account_number = '0690000032'
-        self.ghs_to_ngn_transaction.outflow.save()
+        self.transaction.outflow.dest_account_provider_code = '044'
+        self.transaction.outflow.dest_account_number = '0690000032'
+        self.transaction.outflow.save()
+
+""" class TestBTCToNGNTransaction(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.transaction = Transaction.objects.create(transaction_id='CSHP201208155232267520')
+
+        # Create inflow
+        Inflow.objects.create(
+            transaction=self.btc_to_ngn_transaction,
+            currency='BTC',
+            amount=Decimal('0.0001'),
+        )
+
+        # Create outflow
+        Outflow.objects.create(
+            transaction=self.btc_to_ngn_transaction,
+            currency='NGN',
+            amount=Decimal('734.133'),
+        )
+
+    def tearDown(self):
+        self.transaction.delete()
+
+class UpdatedTestBTCToNGNTransaction(TestBTCToNGNTransaction, TestCustomer):
+    def setUp(self):
+        super().setUp()
+        # Update transaction
+        self.transaction.user = self.customer.user
+        self.transaction.save()
+
+        # Update outflow
+        self.transaction.outflow.dest_account_provider_code = '044'
+        self.transaction.outflow.dest_account_number = '0690000035'
+        self.transaction.outflow.save() """
--- transaction/tests/tests.py
+++ transaction/tests/tests.py
@@ -4,49 +4,43 @@ from .data import *
 from . import UpdatedTestNGNToGHSTransaction, UpdatedTestGHSToNGNTransaction
 
 class SaveNairaPaymentInfoTest(APITestCase, UpdatedTestNGNToGHSTransaction):
-    def setUp(self):
-        super().setUp()
-        self.ngn_to_ghs_transaction.user = self.customer.user
-        self.ngn_to_ghs_transaction.save()
-
     def test_GET_cancelled(self):
         url = '{}{}{}'.format(
             '/t/save-naira-payment-info?txref=',
-            self.ngn_to_ghs_transaction.transaction_id, '&cancelled=true')
+            self.transaction.transaction_id, '&cancelled=true')
 
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)        
         self.assertEqual(response.url, '/')
 
-        self.ngn_to_ghs_transaction.refresh_from_db()
-        self.assertEqual(self.ngn_to_ghs_transaction.status, 'Cancelled')
+        self.transaction.refresh_from_db()
+        self.assertEqual(self.transaction.status, 'Cancelled')
 
     def test_POST(self):
         url = '{}{}{}'.format('/t/save-naira-payment-info?txref=',
-            self.ngn_to_ghs_transaction.transaction_id, '&flwref=FLW200801778')
-        data = naira_payment_info(self.ngn_to_ghs_transaction.transaction_id)
+            self.transaction.transaction_id, '&flwref=FLW200801778')
+        data = naira_payment_info(self.transaction.transaction_id)
 
         response = self.client.post(url, data)
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, '/')
 
-        self.ngn_to_ghs_transaction.refresh_from_db()
-        self.assertTrue(self.ngn_to_ghs_transaction.inflow.reference)
+        self.transaction.refresh_from_db()
+        self.assertTrue(self.transaction.inflow.reference)
 
 class HandleNairaPaymentUpdateTest(APITestCase, UpdatedTestNGNToGHSTransaction):
     def setUp(self):
         super().setUp()
         self.url = '/t/handle-naira-update'
-        self.data = naira_payment_update_card(self.ngn_to_ghs_transaction.transaction_id)
+        self.data = naira_payment_update_card(self.transaction.transaction_id)
 
     def test_POST(self):
-        transaction = self.ngn_to_ghs_transaction
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, 200)
 
         # Check that inflow is updated
-        inflow = transaction.inflow
+        inflow = self.transaction.inflow
         inflow.refresh_from_db()
         self.assertEqual(inflow.source_account_provider, 'card')
         self.assertEqual(inflow.source_account_number, '539983******9335')
@@ -57,7 +51,6 @@ class HandleCediTransferUpdate(APITestCase, UpdatedTestNGNToGHSTransaction):
     def setUp(self):
         super().setUp()
         self.url = '/t/handle-cedi-transfer-update'
-        self.transaction = self.ngn_to_ghs_transaction
 
     def test_POST(self):
         data = cedi_transfer_update(self.transaction.transaction_id)
@@ -73,50 +66,121 @@ class HandleCediTransferUpdate(APITestCase, UpdatedTestNGNToGHSTransaction):
 class SaveCediPaymentInfoTest(APITestCase, UpdatedTestGHSToNGNTransaction):
     def test_GET(self):
         url = '{}{}'.format('/t/save-cedi-payment-info?',
-            cedi_payment_info(self.transaction_id))
+            cedi_payment_info(self.transaction.transaction_id))
 
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, '/')
 
-        self.ghs_to_ngn_transaction.refresh_from_db()
-        self.assertTrue(self.ghs_to_ngn_transaction.inflow.reference)
-        self.assertTrue(self.ghs_to_ngn_transaction.inflow.source_account_number)
-        self.assertTrue(self.ghs_to_ngn_transaction.inflow.source_account_provider)
+        self.transaction.refresh_from_db()
+        self.assertTrue(self.transaction.inflow.reference)
+        self.assertTrue(self.transaction.inflow.source_account_number)
+        self.assertTrue(self.transaction.inflow.source_account_provider)
 
 class HandleCediPaymentUpdateTest(APITestCase, UpdatedTestGHSToNGNTransaction):
     def setUp(self):
         super().setUp()
         self.url = '/t/handle-cedi-payment-update'
-        self.data = cedi_payment_update(
-            self.ghs_to_ngn_transaction.transaction_id)
+        self.data = cedi_payment_update(self.transaction.transaction_id)
+        print(self.transaction)
+        print(self.transaction.outflow)
 
     def test_POST(self):
-        transaction = self.ghs_to_ngn_transaction
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, 200)
 
         # Check that inflow and outflow are updated
-        transaction.inflow.refresh_from_db()
-        self.assertTrue(transaction.inflow.is_complete)
-        self.assertTrue(transaction.inflow.updated_at)
+        self.transaction.inflow.refresh_from_db()
+        self.assertTrue(self.transaction.inflow.is_complete)
+        self.assertTrue(self.transaction.inflow.updated_at)
 
-        transaction.outflow.refresh_from_db()
-        self.assertTrue(transaction.outflow.reference)
+        self.transaction.outflow.refresh_from_db()
+        self.assertTrue(self.transaction.outflow.reference)
 
 class HandleNairaTransferUpdate(APITestCase, UpdatedTestGHSToNGNTransaction):
     def setUp(self):
         super().setUp()
         self.url = '/t/handle-naira-update'
-        self.data = naira_transfer_update(self.ghs_to_ngn_transaction.transaction_id)
+        self.data = naira_transfer_update(self.transaction.transaction_id)
 
     def test_POST(self):
-        transaction = self.ghs_to_ngn_transaction
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, 200)
 
-        transaction.refresh_from_db()
-        self.assertTrue(transaction.outflow.is_complete)
-        self.assertTrue(transaction.is_complete)
-        self.assertEqual(transaction.status, 'Successful')
+        self.transaction.refresh_from_db()
+        self.assertTrue(self.transaction.outflow.is_complete)
+        self.assertTrue(self.transaction.is_complete)
+        self.assertEqual(self.transaction.status, 'Successful')
+
+""" class HandleBTCPaymentUpdate(APITestCase, UpdatedTestBTCToNGNTransaction):
+    def setUp(self):
+        super().setUp()
+        # Create transactions
+        ngn_data = copy.deepcopy(self.ngn_transaction_data)
+        ngn_data.update({
+            # let's use a random id so we can
+            # get valid response from Rave
+            # every time.
+            'id': random.randint(1, 10000),
+            'user': self.user,
+            'dest_account': self.bank_account,
+            'invoice_created_at': timezone.now()
+        })
+        ghs_data = copy.deepcopy(self.ghs_transaction_data)
+        ghs_data.update({
+            # let's use a random id so we can
+            # get valid response from Rave
+            # every time.
+            'id': random.randint(1, 10000),
+            'user': self.user,
+            'dest_account': self.wallet,
+            'invoice_created_at': timezone.now()
+        })
+        self.ngn_transaction = Transaction.objects.create(**ngn_data)
+        self.ghs_transaction = Transaction.objects.create(**ghs_data)
+
+    def _get_data(self, pk, url):
+        return {
+            'id': '0f0008fb-9aca-452c-a355-74b1c526dda3',
+            'callback_url': '{}{}'.format('http://185.20.49.94:25155', url),
+            'success_url': 'http://185.20.49.94:25155/activity',
+            'status': 'paid',
+            'order_id': pk,
+            'user_id': self.user.pk,
+            'description': 'BTC to NGN',
+            'price': '100000',
+            'fee': '0',
+            'auto_settle': '0',
+            'hashed_order': '3d1ee193b12bf9b3343e69b31dccc8787c0278cc4e8897f021640a5e9d12fe40'
+        }
+
+    def test_update_source_invoice_paid_ngn_transaction(self):
+        url = '{}{}{}'.format('/t/', self.ngn_transaction.pk, '/update-source')
+        data = self._get_data(self.ngn_transaction.pk, url)
+
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'{"message":"Success"}')
+
+        t = Transaction.objects.get(pk=self.ngn_transaction.pk)
+        self.assertTrue(t.source_debited_at is not None)
+        self.assertEqual(t.source_transaction_id, data['id'])
+        self.assertTrue(t.transfer_initiated_at is not None)
+
+    def test_update_source_invoice_paid_ghs_transaction(self):
+        url = '{}{}{}'.format('/t/', self.ghs_transaction.pk, '/update-source')
+        data = self._get_data(self.ghs_transaction.pk, url)
+
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'{"message":"Success"}')
+
+        t = Transaction.objects.get(pk=self.ghs_transaction.pk)
+        self.assertTrue(t.source_debited_at is not None)
+        self.assertEqual(t.source_transaction_id, data['id'])
+        self.assertTrue(t.transfer_initiated_at is not None)
+
+    def tearDown(self):
+        self.ngn_transaction.delete()
+        self.ghs_transaction.delete() """
